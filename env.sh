9138f5eda6ca22df8b08bb86d688783d91e8f92e
Masao Kitamura <masaok@gmail.com>
2018-08-08 20:37:16 -0700

    adding first unit tests from Google samples

---
+++ env.sh
@@ -0,0 +1,12 @@
+
+export PROJECT_HOME=$HOME/LionHouse
+
+export CLOUD_SDK_HOME=$HOME/google_cloud/google-cloud-sdk
+
+export PYTHONPATH=$PROJECT_HOME/env/lib/python2.7
+export PYTHONPATH=$PYTHONPATH:$PROJECT_HOME/env/lib/python2.7/site-packages
+export PYTHONPATH=$PYTHONPATH:$CLOUD_SDK_HOME/lib
+export PYTHONPATH=$PYTHONPATH:$CLOUD_SDK_HOME/lib/third_party
+export PYTHONPATH=$PYTHONPATH:$CLOUD_SDK_HOME/platform/google_appengine
+export PYTHONPATH=$PYTHONPATH:$PROJECT_HOME/etc
+
--- tests/User_test.py
+++
@@ -1,8 +0,0 @@
-
-
-import unittest
-from google.appengine.ext import testbed, ndb
-from mock import patch, Mock
-
-import LionHouse_models.User as User
-
