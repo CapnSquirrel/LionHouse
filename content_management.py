ae3d7938218c5f8d7cb7fdb489332d81c85d779f
Alejandro Zapata <keybladeschosen1@hotmail.com>
2018-08-09 21:30:24 -0700

    Bit of a messy implementation for post formatting, but it works

--- content_management.py
+++ content_management.py
@@ -1,9 +1,22 @@
 from google.appengine.api import users
 from google.appengine.ext import ndb
 from LionHouse_models import Post, User
+from datetime import datetime
 
 logout_url = users.create_logout_url('/')
 login_url = users.create_login_url('/')
+class PPost():
+    def __init__(self, author, content, time):
+        self.author = author
+        self.content = content
+        self.time = time
+
+def format_posts(posts):
+    formatted_posts = []
+    for post in posts:
+        author = User.query().filter(User.key == post.author).get().username
+        formatted_posts.append(PPost(author, post.content, post.time))
+    return formatted_posts
 
 def populate_feed(current_user):
     feed_fields = {
@@ -12,6 +25,6 @@ def populate_feed(current_user):
         "user_name": current_user.name,
         "post_count": len(Post.query().filter(Post.author == current_user.key).fetch()),
         "user_count": len(User.query().fetch()),
-        "test_post": Post.query().order(-Post.time).fetch()
+        "posts": format_posts(Post.query().order(-Post.time).fetch(limit=20)),
     }
     return feed_fields
